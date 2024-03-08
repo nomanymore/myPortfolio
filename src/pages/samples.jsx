9fd86472dea6b858bfafce40179ce6a469ad10b8
Victoria Shmakov <nomanymore5@gmail.com>
2024-03-07 20:52:50 -0400

    width changed

--- src/pages/samples.jsx
+++ src/pages/samples.jsx
@@ -187,7 +187,7 @@ export default function Samples() {
                 )}
 
                 {selectedSection === 'data' && (
-                    <div className="w-3/4 mx-auto">
+                    <div className="w-85% mx-auto">
                         <div className="bg-white bg-opacity-90 p-4 m-4 rounded-xl shadow-lg w-1/2 ">
                             <iframe title="NY resolutions" width="493" height="228" src="https://app.powerbi.com/view?r=eyJrIjoiOWVmMWE2OGQtMDExMC00N2RmLTk1YTEtOWQ2Mzc2MTA5NWQyIiwidCI6IjlmMjlkOTFmLWYzM2EtNGY4NC05ZDU2LWE0NGQ4ODk0YTA0NiJ9&embedImagePlaceholder=true&pageName=ReportSection" frameborder="0" allowFullScreen="true"></iframe>
                             <h3 className={`text-2xl font-black text-red-800 mt-2 ${cinzel.className}`}>NY Resolutions</h3>
